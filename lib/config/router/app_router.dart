fe3e18fe64f3db120313c63e7dfefc28f3d8d1be
Sameh Ibrahim <131698428+SamehKhalilIbrahim@users.noreply.github.com>
2026-01-05 21:11:25 +0200

    ..

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -20,7 +20,7 @@ abstract class AppRouter {
   static const String ahadithPath = "/ahadith";
 
   static final router = GoRouter(
-    initialLocation: mushafPath,
+    initialLocation: surahListPath,
     routes: [
       GoRoute(
         path: homePath,
@@ -53,7 +53,7 @@ abstract class AppRouter {
         path: mushafPath,
         pageBuilder: GoTransitions.fade.withFade.build(
           builder: (context, state) {
-            final int pageNo = (state.extra as int?) ?? 543;
+            final int pageNo = (state.extra as int?) ?? 1;
 
             return MushafPage(pageNumber: pageNo);
           },
