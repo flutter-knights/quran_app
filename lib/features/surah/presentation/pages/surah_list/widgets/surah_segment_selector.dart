f3b038c167dfcb943123443d7d0cc679e77710b6
Sameh Ibrahim <131698428+SamehKhalilIbrahim@users.noreply.github.com>
2026-01-08 10:08:15 +0200

    feat: refactor SurahSegmentSelector

--- lib/features/surah/presentation/pages/surah_list/widgets/surah_segment_selector.dart
+++ lib/features/surah/presentation/pages/surah_list/widgets/surah_segment_selector.dart
@@ -17,63 +17,91 @@ class _SurahSegmentSelectorState extends State<SurahSegmentSelector> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        final totalWidth = constraints.maxWidth;
-        final segmentWidth = totalWidth / options.length;
-
-        return Container(
-          height: 36,
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
-          decoration: BoxDecoration(
-            color: context.colorScheme.surfaceContainer,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Stack(
-            children: [
-              AnimatedPositioned(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                top: 0,
-                bottom: 0,
-                left: context.isArabic ? null : segmentWidth * selected,
-                right: context.isArabic ? segmentWidth * selected : null,
-                width: segmentWidth - 8,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: context.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                ),
+    return Container(
+      height: 36,
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      decoration: BoxDecoration(
+        color: context.colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            left: context.isArabic ? null : _calculatePosition(selected),
+            right: context.isArabic ? _calculatePosition(selected) : null,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: _calculateWidth(selected),
+              decoration: BoxDecoration(
+                color: context.colorScheme.primary,
+                borderRadius: BorderRadius.circular(4),
               ),
-              Row(
-                children: List.generate(options.length, (index) {
-                  final bool isSelected = index == selected;
+            ),
+          ),
+          Row(
+            children: List.generate(options.length, (index) {
+              final bool isSelected = index == selected;
+              return Expanded(
+                child: GestureDetector(
+                  onTap: () => setState(() => selected = index),
+                  child: Container(
+                    padding: .only(
+                      left: context.isArabic
+                          ? index == 3
+                                ? 0
+                                : 30
+                          : 0,
+                      right: !context.isArabic
+                          ? index == 3
+                                ? 0
+                                : 30
+                          : 0,
+                    ),
 
-                  return Expanded(
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () => setState(() => selected = index),
-                      child: Center(
-                        child: Text(
-                          options[index],
-                          style: TS.semi12.copyWith(
-                            color: isSelected
-                                ? context.colorScheme.onPrimary
-                                : context.colorScheme.onSurface.withValues(
-                                    alpha: 0.7,
-                                  ),
-                          ),
+                    child: Center(
+                      child: Text(
+                        options[index],
+                        style: TS.semi12.copyWith(
+                          color: isSelected
+                              ? context.colorScheme.onPrimary
+                              : context.colorScheme.onSurface.withValues(
+                                  alpha: 0.7,
+                                ),
                         ),
                       ),
                     ),
-                  );
-                }),
-              ),
-            ],
+                  ),
+                ),
+              );
+            }),
           ),
-        );
-      },
+        ],
+      ),
     );
+  }
+
+  double _calculatePosition(int index) {
+    final size = MediaQuery.maybeOf(context)?.size;
+    if (size == null || size.width == 0) {
+      return 0;
+    }
+
+    final containerWidth = size.width - 32;
+    final segmentWidth = containerWidth / options.length;
+    return (segmentWidth * index) + 4;
+  }
+
+  double _calculateWidth(int index) {
+    final size = MediaQuery.maybeOf(context)?.size;
+    if (size == null || size.width == 0) {
+      return 50;
+    }
+
+    final containerWidth = size.width - 32;
+    final segmentWidth = containerWidth / options.length;
+    return segmentWidth - (index == 3 ? 8 : 38);
   }
 }
