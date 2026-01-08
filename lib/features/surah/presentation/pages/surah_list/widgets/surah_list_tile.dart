231191ce9f9fca05eddbed6fc549e86ff1052808
Sameh Ibrahim <131698428+SamehKhalilIbrahim@users.noreply.github.com>
2026-01-08 09:54:22 +0200

    ...

--- lib/features/surah/presentation/pages/surah_list/widgets/surah_list_tile.dart
+++ lib/features/surah/presentation/pages/surah_list/widgets/surah_list_tile.dart
@@ -33,7 +33,7 @@ class SurahListTile extends StatelessWidget {
           ).push(AppRouter.mushafPath, extra: surah.pageNumber);
         },
         child: AspectRatio(
-          aspectRatio: 4.9,
+          aspectRatio: 4.8,
 
           child: Container(
             padding: .all(8),
