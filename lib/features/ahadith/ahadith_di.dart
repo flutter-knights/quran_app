0aff870eca65a4f65197fc68a78e056b57d4cd94
savior131 <aa8861336@gmail.com>
2026-05-28 09:03:47 +0300

    feat(ahadith): register hadith bookmark + next-hadith dependencies

--- lib/features/ahadith/ahadith_di.dart
+++ lib/features/ahadith/ahadith_di.dart
@@ -7,12 +7,19 @@ import 'package:quran_app/core/di/dependency_injection.dart';
 import 'package:quran_app/features/ahadith/data/datasources/local/ahadith_local_data_source.dart';
 import 'package:quran_app/features/ahadith/data/datasources/remote/ahadith_remote_data_source.dart';
 import 'package:quran_app/features/ahadith/data/models/hadith_hive_model.dart';
+import 'package:quran_app/features/ahadith/data/datasources/local/hadith_bookmark_local_data_source.dart';
 import 'package:quran_app/features/ahadith/data/repositories/ahadith_repository_impl.dart';
+import 'package:quran_app/features/ahadith/data/repositories/hadith_bookmark_repository_impl.dart';
 import 'package:quran_app/features/ahadith/domain/repositories/ahadith_repository.dart';
+import 'package:quran_app/features/ahadith/domain/repositories/hadith_bookmark_repository.dart';
 import 'package:quran_app/features/ahadith/domain/usecases/download_ahadith_book_use_case.dart';
 import 'package:quran_app/features/ahadith/domain/usecases/get_ahadith_page_use_case.dart';
+import 'package:quran_app/features/ahadith/domain/usecases/get_hadith_bookmarks.dart';
+import 'package:quran_app/features/ahadith/domain/usecases/get_next_hadith.dart';
+import 'package:quran_app/features/ahadith/domain/usecases/toggle_hadith_bookmark.dart';
 import 'package:quran_app/features/ahadith/presentation/cubit/ahadith_cubit.dart';
 import 'package:quran_app/features/ahadith/presentation/cubit/download_book_cubit.dart';
+import 'package:quran_app/features/ahadith/presentation/cubit/hadith_bookmark_cubit.dart';
 
 void initAhadith() async {
   final hadithBox = Hive.box<HadithHiveModel>('ahadithCache');
@@ -49,6 +56,29 @@ void initAhadith() async {
   sl.registerFactory<DownloadBookCubit>(
     () => DownloadBookCubit(downloadAhadithBookUseCase: sl()),
   );
+
+  // Hadith bookmarks
+  sl.registerLazySingleton<HadithBookmarkLocalDataSource>(
+    () => HadithBookmarkLocalDataSource(
+      box: Hive.box<List>('hadith_bookmarks'),
+    ),
+  );
+  sl.registerLazySingleton<HadithBookmarkRepository>(
+    () => HadithBookmarkRepositoryImpl(dataSource: sl()),
+  );
+  sl.registerLazySingleton<GetHadithBookmarks>(() => GetHadithBookmarks(sl()));
+  sl.registerLazySingleton<ToggleHadithBookmark>(
+    () => ToggleHadithBookmark(sl()),
+  );
+  sl.registerLazySingleton<HadithBookmarkCubit>(
+    () => HadithBookmarkCubit(
+      getBookmarks: sl(),
+      toggleBookmark: sl(),
+    ),
+  );
+
+  // Next hadith navigation
+  sl.registerLazySingleton<GetNextHadith>(() => GetNextHadith(sl()));
 }
 
 Future<Map<String, dynamic>> getChapters() async {
